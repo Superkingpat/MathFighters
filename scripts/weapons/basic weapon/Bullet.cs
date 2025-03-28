ecdf77e3bcf3486cdda5d8576f00e709172259d2
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2025-03-28 17:01:29 +0100

    The bullet is now deleted when it exits the screen. Also added comments to the code

--- scripts/weapons/basic weapon/Bullet.cs
+++ scripts/weapons/basic weapon/Bullet.cs
@@ -1,22 +1,37 @@
 using Godot;
 using System;
 
+//This is the Bullet class
 public partial class Bullet : CharacterBody2D {
+	//Speed is an external function that we can change in the Godot UI
 	[Export] public float Speed = 500.0f;
 	private Vector2 direction;
 
+	//_Ready is called when the root node (Bullet) entered the scene
+	//VisibleOnScreenNotifier2D has multiple functionalities that tell us if an object is on the screen
+	//All actions(functions) in notifier.ScreenExited are executed when the object has exited the screen
+	public override void _Ready() {
+		VisibleOnScreenNotifier2D notifier = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
+		notifier.ScreenExited += OnScreenExited;
+	}
+
+	//Its an initialization function. I think it's obvious what it does
 	public void Init(Vector2 targetPosition, Vector2 startPosition) {
 		Position = startPosition;
 		direction = (targetPosition - startPosition).Normalized();
 		Rotation = direction.Angle();
 	}
 
+	//Moves the Bullet in the direction of direction at the speed of speed. Delta time is taken care of automaticly by Godot
 	public override void _PhysicsProcess(double delta) {
 		Velocity = direction * Speed;
 		MoveAndSlide();
 	}
 
+	//Is called when the Bullet instance exits the screen.
 	private void OnScreenExited() {
+		GD.Print("Bullet was deleted!");
+		//Deletes the Bullet instance at the end of the current frame
 		QueueFree();
 	}
 }
