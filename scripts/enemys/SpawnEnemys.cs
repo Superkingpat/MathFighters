9b9a37e324d498f6627fde8bb6f1178c6f860c04
Remixer611 <drevo.deska@gmail.com>
2025-04-10 20:35:52 +0200

    Added enemy spawnig

---
+++ scripts/enemys/SpawnEnemys.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class SpawnEnemys : Node
+{
+	// Drag and drop your enemy scene into this field in the editor
+	[Export]
+	public PackedScene EnemyScene;
+
+	// You can also expose a position for spawning (optional)
+	[Export]
+	public Vector2 SpawnPosition = new Vector2(100, 100);
+
+	public override void _Ready()
+	{
+		// You can call it here to test
+		SpawnEnemy();
+	}
+
+	public void SpawnEnemy()
+	{
+		if (EnemyScene == null)
+		{
+			GD.PrintErr("EnemyScene is not assigned!");
+			return;
+		}
+
+		Node2D enemyInstance = (Node2D)EnemyScene.Instance(); // Or use Node3D depending on your enemy
+		enemyInstance.Position = SpawnPosition;
+		AddChild(enemyInstance);
+	}
+}
